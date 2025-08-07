acebe1da4796d48f32a9aea9d61c30ef641cef80
Yaroslav Shevchuk <yarolegovich@gmail.com>
2025-08-07 14:41:47 +0000

    shorten method name

--- a2asrv/handler.go
+++ a2asrv/handler.go
@@ -21,22 +21,22 @@ import (
 	"github.com/a2aproject/a2a-go/a2a"
 )
 
-type RequestHandler interface {
-	OnHandleGetTask(ctx context.Context, query a2a.TaskQueryParams) (a2a.Task, error)
+type Requestr interface {
+	OnGetTask(ctx context.Context, query a2a.TaskQueryParams) (a2a.Task, error)
 
-	OnHandleCancelTask(ctx context.Context, id a2a.TaskIDParams) (a2a.Task, error)
+	OnCancelTask(ctx context.Context, id a2a.TaskIDParams) (a2a.Task, error)
 
-	OnHandleSendMessage(ctx context.Context, message a2a.MessageSendParams) (a2a.SendMessageResult, error)
+	OnSendMessage(ctx context.Context, message a2a.MessageSendParams) (a2a.SendMessageResult, error)
 
-	OnHandleResubscribeToTask(ctx context.Context, id a2a.TaskIDParams) iter.Seq2[a2a.Event, error]
+	OnResubscribeToTask(ctx context.Context, id a2a.TaskIDParams) iter.Seq2[a2a.Event, error]
 
-	OnHandleSendMessageStream(ctx context.Context, message a2a.MessageSendParams) iter.Seq2[a2a.Event, error]
+	OnSendMessageStream(ctx context.Context, message a2a.MessageSendParams) iter.Seq2[a2a.Event, error]
 
-	OnHandleGetTaskPushConfig(ctx context.Context, params a2a.GetTaskPushConfigParams) (a2a.TaskPushConfig, error)
+	OnGetTaskPushConfig(ctx context.Context, params a2a.GetTaskPushConfigParams) (a2a.TaskPushConfig, error)
 
-	OnHandleListTaskPushConfig(ctx context.Context, params a2a.ListTaskPushConfigParams) ([]a2a.TaskPushConfig, error)
+	OnListTaskPushConfig(ctx context.Context, params a2a.ListTaskPushConfigParams) ([]a2a.TaskPushConfig, error)
 
-	OnHandleSetTaskPushConfig(ctx context.Context, params a2a.TaskPushConfig) (a2a.TaskPushConfig, error)
+	OnSetTaskPushConfig(ctx context.Context, params a2a.TaskPushConfig) (a2a.TaskPushConfig, error)
 
-	OnHandleDeleteTaskPushConfig(ctx context.Context, params a2a.DeleteTaskPushConfigParams) error
+	OnDeleteTaskPushConfig(ctx context.Context, params a2a.DeleteTaskPushConfigParams) error
 }

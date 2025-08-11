de331b08e6a95d1dd281d8e100adb03e52ad2b88
Yaroslav Shevchuk <yarolegovich@gmail.com>
2025-08-11 13:11:18 +0000

    remove licences

--- a2apb/doc.go
+++ a2apb/doc.go
@@ -12,6 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package a2apb contains types and interfaces generated from published
-// official A2A .proto specification.
 package a2apb
